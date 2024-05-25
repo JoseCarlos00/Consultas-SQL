c345f5a1f770a52a96c628e73f770ba01860462a
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2024-05-25 08:19:21 -0600

    Update UPDATE Secuencia de Surtido.sql
    Separar en dos case el picking_seq y putaway_seq

--- Otras/UPDATE Secuencia de Surtido.sql
+++ Otras/UPDATE Secuencia de Surtido.sql
@@ -1,17 +1,19 @@
 UPDATE LOCATION
 SET 
-    (picking_seq, putaway_seq) = 
-    CASE
-        -- WHEN location = 'LOCATION' THEN ('372125', '372125')
-
-  
-        ELSE ('372125', '372125')
+    picking_seq = CASE
+       --  WHEN LOCATION = '1-10-08-DD-11' THEN '41500'
+        
+        ELSE '41500'
+    END,
+    
+    putaway_seq = CASE
+        --WHEN LOCATION = '1-10-08-DD-11' THEN '41500'
+        
+        ELSE '41500'
     END
-
 WHERE 
     warehouse = 'Mariano' 
     AND work_zone = 'W-Mar Bodega 2'
     AND location IN (
-      -- 'LOCATION'
-
+        -- 'LOCATION',
     );
