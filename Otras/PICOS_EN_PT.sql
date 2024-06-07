5379f001b4b8444ed0e14bcfbb87d1c0e19fe67e
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2024-06-07 08:14:48 -0600

    Update PICOS_EN_PT.sql
    Se agrgaron las columnas de WORK_ZONE y GRUPO

--- Otras/PICOS_EN_PT.sql
+++ Otras/PICOS_EN_PT.sql
@@ -1,64 +1,56 @@
-SELECT 
-  WAREHOUSE, LOCATION, ITEM, ITEM_COLOR, COMPANY,
+SELECT  
+  WAREHOUSE,
+  WORK_ZONE,
+  LOCATION,
+  ITEM,
+  ITEM_COLOR,
+  COMPANY,
   CAST(SUM(AV) AS INT) AS AV,
   CAST(SUM(OH) AS INT) AS OH,
   CAST(SUM(AL) AS INT) AS AL,
   CAST(SUM(IT) AS INT) AS IT,
   CAST(SUM(SU) AS INT) AS SU,
-  HUELLA,
-  CAJAS
+  CAST(HUELLA AS INT) AS HUELLA,
+  CAJAS,
+  GRUPO
 
-FROM 
-(
-SELECT
-  LI.WAREHOUSE,
-  LI.LOCATION,
-  LI.ITEM,
-  LI.ITEM_COLOR,
-  LI.COMPANY,
-  ((LI.ON_HAND_QTY + LI.IN_TRANSIT_QTY) -  (LI.ALLOCATED_QTY + LI.SUSPENSE_QTY)) AS AV,
-  LI.ON_HAND_QTY AS OH,
-  LI.ALLOCATED_QTY AS AL,
-  LI.IN_TRANSIT_QTY AS IT,
-  LI.SUSPENSE_QTY AS SU,
-  LI.internal_location_inv,
-  UOM.conversion_qty AS HUELLA,
-  CAST((LI.on_hand_qty / UOM.conversion_qty) AS DECIMAL(5, 2)) AS CAJAS
- 
+FROM (
+	SELECT  
+    LI.WAREHOUSE,
+    L.WORK_ZONE,
+    LI.LOCATION,
+    LI.ITEM,
+    LI.ITEM_COLOR,
+    LI.COMPANY,
+    ((LI.ON_HAND_QTY + LI.IN_TRANSIT_QTY) - (LI.ALLOCATED_QTY + LI.SUSPENSE_QTY)) AS AV,
+    LI.ON_HAND_QTY                                                                AS OH,
+    LI.ALLOCATED_QTY                                                              AS AL,
+    LI.IN_TRANSIT_QTY                                                             AS IT,
+    LI.SUSPENSE_QTY                                                               AS SU,
+    UOM.conversion_qty                                                            AS HUELLA,
+    CAST((LI.on_hand_qty / UOM.conversion_qty) AS DECIMAL(5,2))                   AS CAJAS,
+    I.ITEM_CATEGORY4                                                              AS GRUPO,
+    LI.internal_location_inv
 
-FROM location_inventory LI
-INNER JOIN location L
- ON L.location = LI.location
+	FROM location_inventory LI
+	INNER JOIN location L ON L.location = LI.location
+	INNER JOIN Item_unit_of_measure UOM ON LI.item = UOM.item
+	INNER JOIN item I ON I.item = LI.item
 
-INNER JOIN Item_unit_of_measure UOM
- ON LI.item = UOM.item
+	WHERE LI.warehouse = 'Tultitlan'
+	AND UOM.sequence = '2' AND I.company = 'FM'
 
-WHERE LI.warehouse='Tultitlan'
-AND L.work_zone = 'W-Tul Producto Terminado'
-AND UOM.sequence = '2'
+	AND L.WORK_ZONE = 'W-Tul Producto Terminado'
+	-- AND (L.WORK_ZONE = 'W-Tul Producto Terminado' OR L.WORK_ZONE LIKE '%PICOS%') 
+  -- AND (I.ITEM_CATEGORY4 LIKE '%NAV%' OR I.ITEM_CATEGORY4 LIKE '%MUERTOS%' )
 
-
-GROUP BY LI.WAREHOUSE, LI.LOCATION, LI.ITEM,
-  LI.ITEM_COLOR,
-  LI.COMPANY,
-  LI.ON_HAND_QTY,
-  LI.ALLOCATED_QTY,
-  LI.IN_TRANSIT_QTY,
-  LI.SUSPENSE_QTY,
-  LI.internal_location_inv,
-  LI.logistics_Unit,
-  UOM.sequence,
-   UOM.conversion_qty
-
+	GROUP BY  
+    LI.WAREHOUSE, LI.LOCATION, LI.ITEM, LI.ITEM_COLOR, LI.COMPANY, LI.ON_HAND_QTY, LI.ALLOCATED_QTY, LI.IN_TRANSIT_QTY, LI.SUSPENSE_QTY, LI.internal_location_inv, LI.logistics_Unit, UOM.sequence, UOM.conversion_qty, I.company, I.ITEM_CATEGORY4, L.WORK_ZONE
 ) AS PRINCIPAL
 
 WHERE CAJAS <> ROUND(CAJAS, 0)
-AND CAJAS < 1
+-- AND CAJAS < 1 
 
-
-
-GROUP BY WAREHOUSE, LOCATION, ITEM, ITEM_COLOR, COMPANY, HUELLA, CAJAS
-
-ORDER BY 3, 2
-
--- WAREHOUSE,LOCATION,ITEM,ITEM_COLOR,COMPANY,AV,OH,AL,IT,SU,HUELLA,CAJAS,
+GROUP BY  WAREHOUSE, WORK_ZONE, LOCATION, ITEM, ITEM_COLOR, COMPANY, HUELLA, CAJAS, GRUPO
+ORDER BY  WORK_ZONE, ITEM
+-- WAREHOUSE, WORk_ZONE, LOCATION, ITEM, ITEM_COLOR, COMPANY, AV, OH, AL, IT, SU, HUELLA, CAJAS, GRUPO, 
