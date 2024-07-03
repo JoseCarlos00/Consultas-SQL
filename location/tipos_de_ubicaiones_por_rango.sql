d60db7e7412ae333d32d3a68e4226d506e1f1ea5
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2024-07-03 11:04:04 -0600

    Update tipos_de_ubicaiones_por_rango.sql

--- location/tipos_de_ubicaiones_por_rango.sql
+++ location/tipos_de_ubicaiones_por_rango.sql
@@ -1,14 +1,11 @@
 SELECT 
     L.location, L.location_type, L.allocation_zone, L.work_zone, L.locating_zone, L.multi_item, L.track_containers, L.location_sts AS status
-
 FROM location L
 
 WHERE L.warehouse = 'Mariano'
-AND L.location >= '1-90-01-AA-01'
-AND L.location <= '1-90-05-AA-01'
-AND L.location  LIKE '1%' 
-AND L.work_zone LIKE 'W-Mar Bodega%'
-
+AND L.location >= '1-90-01%'
+AND L.location <= '1-90-05%'
+--AND L.location LIKE '%BB%'
 
 -- LOCATION,LOCATION_TYPE,ALLOCATION_ZONE,WORK_ZONE,LOCATING_ZONE,MULTI_ITEM,TRACK_CONTAINER,STATUS
 
