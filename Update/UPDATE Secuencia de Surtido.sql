21df2f840885a1005d2c105be4ffc4a9afe388c4
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2024-07-23 14:20:17 -0600

    Update UPDATE Secuencia de Surtido.sql

--- Update/UPDATE Secuencia de Surtido.sql
+++ Update/UPDATE Secuencia de Surtido.sql
@@ -3,13 +3,13 @@ SET
     picking_seq = CASE
        --  WHEN LOCATION = '1-10-08-DD-11' THEN '41500'
         
-        ELSE '41500'
+        ELSE null
     END,
     
     putaway_seq = CASE
         --WHEN LOCATION = '1-10-08-DD-11' THEN '41500'
         
-        ELSE '41500'
+        ELSE null
     END
 WHERE 
     warehouse = 'Mariano' 
