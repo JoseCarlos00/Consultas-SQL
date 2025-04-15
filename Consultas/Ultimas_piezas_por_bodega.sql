93a562f092992e82f6fe74b097753a7c280c3525
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-04-15 07:34:03 -0600

    Add: ultimas piezas

---
+++ Consultas/Ultimas_piezas_por_bodega.sql
@@ -0,0 +1,47 @@
+SELECT DISTINCT
+  L.work_zone,
+  L.location,
+  I.item,
+  I.ITEM_COLOR,
+  LI.ON_HAND_QTY,
+  LI.ALLOCATED_QTY
+
+
+FROM location_inventory LI
+  LEFT OUTER JOIN item_location_capacity ILC ON ILC.item = LI.item
+  INNER JOIN location L ON L.location = LI.location
+  INNER JOIN item I ON I.item = LI.item AND I.company = 'FM'
+
+WHERE LI.warehouse = 'Mariano'
+  AND L.warehouse = 'Mariano'
+  AND (
+    ILC.location_type NOT LIKE 'Generica Permanente R'
+    OR ILC.location_type IS NULL
+  )
+  AND L.location_type = 'Generica Permanente S' -- Verificar si el item existe en el inventario
+  AND LI.ITEM NOT IN (
+    SELECT DISTINCT LI.item
+    FROM location_inventory LI
+      INNER JOIN LOCATION L ON L.location = LI.location
+    WHERE LI.company = 'FM'
+      AND LI.warehouse = 'Mariano'
+      AND L.warehouse = 'Mariano'
+      AND (L.location_type<>'Muelle' OR L.location IN ('PRE-01', 'REC-01'))
+      AND L.location_class<>'Shipping Dock' 
+      AND (L.location_type<>'Piso' OR L.location IN ('ELEVADOR', 'REC-01', 'HOT-01', 'HOT-02', 'LISTONES-00', 'LISTONES-01', 'DEVOLUCIONES'))
+      AND (
+        L.work_zone <> 'W-Mar Bodega 6' -- Cualquier ubicación fuera de Bodega 6
+        OR (
+          L.work_zone = 'W-Mar Bodega 6'
+          AND L.location_type NOT IN ('Generica Permanente S', 'Generica Dinamico S')
+        )
+      )
+      AND LI.on_hand_qty > 0
+  )
+  AND L.work_zone = 'W-Mar Bodega 6'
+
+AND  LI.ON_HAND_QTY <= 25
+AND  LI.ON_HAND_QTY > 0
+
+ORDER BY L.location
+-- WORK_ZONE,LOCATION,ITEM,ITEM_COLOR,OH,AL,
