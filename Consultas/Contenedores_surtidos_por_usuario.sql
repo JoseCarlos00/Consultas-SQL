b42ce14a448964562ef04f0a79b008a1fc14d66e
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-05-30 07:57:38 -0600

    Update

---
+++ Consultas/Contenedores_surtidos_por_usuario.sql
@@ -0,0 +1,47 @@
+SELECT
+    ROW_NUMBER() OVER (ORDER BY TH.internal_id) AS NumeroDeFila,
+    TH.internal_id,
+    TH.reference_id,
+    TH.activity_date_time,
+    TH.location,
+    TH.quantity,
+    TH.item,
+    I.item_color,
+    TH.user_name,
+    TH.work_unit,
+    container.parent_container_id,
+    container.quantity AS container_quantity,
+    container.item AS container_item,
+    container.status,
+    container.shipment_id
+FROM transaction_history TH
+LEFT OUTER JOIN (
+    SELECT
+        SC.parent_container_id,
+        SC.quantity,
+        SC.item,
+        SC.status,
+        SH.shipment_id
+    FROM shipping_container SC
+    INNER JOIN shipment_header SH ON SH.internal_shipment_num = SC.internal_shipment_num
+
+    --- CAMBIAR AQUÍ
+    WHERE SH.shipment_id LIKE '361-%'
+     ---- CAMBIAR AQUÍ
+
+) AS container ON container.item = TH.item AND container.quantity = TH.quantity AND container.shipment_id = TH.reference_id
+INNER JOIN item I ON I.item = TH.item AND I.company = 'FM'
+WHERE TH.warehouse = 'Mariano'
+
+    --- CAMBIAR AQUÍ
+  AND TH.user_name = 'Mariano06'
+  AND TH.reference_id LIKE '361-%'
+    ---- CAMBIAR AQUÍ
+
+  AND TH.transaction_type = 130
+  AND TH.direction = 'From'
+  AND TH.work_zone <> 'W-Mar Pick and Drop ELEV'
+  AND TH.work_zone <> 'W-Mar Pick and Drop'
+  AND CAST(TH.activity_date_time AS DATE) = CAST(GETDATE() AS DATE)
+
+-- NO,INTERNAL_ID,REFERENCE_ID,ACTIVITY_DATE_TIME,LOCATION,QUANTITY,ITEM,COLOR,USER_NAME,WORK_UNIT,PARENT_CONTAINER_ID,CONTAINER_QTY,CONTAINER_ITEM,CONTAINER_STS,CONTAINER_SHIPMENT_ID,
---
+++ Consultas/cancelar_inventario.sql
@@ -0,0 +1,16 @@
+SELECT
+SD.item, SD.total_qty,
+CASE 
+  WHEN SD.status1 > 301 AND SD.status1 <= 600 THEN 'EMP-01'
+  WHEN SD.status1 = 650 THEN  'STG-01'
+  ELSE '' END AS TO_LOC,
+ILA.allocation_loc, SD.status1
+
+FROM shipment_detail SD
+INNER JOIN item_location_assignment ILA ON ILA.item = SD.item AND ILA.quantity_um = 'PZ'
+
+WHERE SD.warehouse = 'Mariano'
+AND SD.erp_order LIKE '%-180200'
+-- AND sd.status1 = 650
+
+-- ELIMINAR PEDIDO Y REGRESAR INVENTARIO
