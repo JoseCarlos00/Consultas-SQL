ec4befd00fc5cc819151f38abe70fa82362fd88c
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2025-06-26 08:58:22 -0600

    Cambio de Huella

---
+++ Cambio_de_huella_por_num_recibo.sql
@@ -0,0 +1,8 @@
+UPDATE UOM
+SET UOM.conversion_qty = RD.open_qty
+FROM Item_unit_of_measure UOM
+INNER JOIN RECEIPT_DETAIL RD 
+    ON UOM.item = RD.item 
+   AND UOM.company = RD.company
+WHERE UOM.sequence = 3
+  AND RD.INTERNAL_RECEIPT_NUM = '348736';
