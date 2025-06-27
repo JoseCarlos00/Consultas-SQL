e2553301c3829b8ab1d6fee0a33b67a618ad293a
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2025-06-27 11:49:25 -0600

    Update INVENTARIO_DE_ALMACENAJE.sql

--- INVENTARIO_DE_ALMACENAJE.sql
+++ INVENTARIO_DE_ALMACENAJE.sql
@@ -1,43 +1,32 @@
-SELECT
-    ZONAS.WORK_ZONE AS WORK_ZONE,
-    LI.LOCATION, 
-    LI.ITEM, 
-    REPLACE(LI.ITEM_DESC, ',', '.') AS DESCRIPTION, 
-    LI.COMPANY, 
-    CAST(LI.ON_HAND_QTY AS INT) AS OH,
-    CAST((LI.ON_HAND_QTY/UOM.conversion_qty) AS INT) AS CAJAS,
-    FECHA 
-    --,THR.USER_NAME
-FROM LOCATION_INVENTORY LI
-INNER JOIN Item_unit_of_measure UOM ON LI.ITEM=UOM.item AND UOM.sequence='2' AND UOM.company='FM'
+SELECT 
+  PRINCIPAL.WORK_ZONE,PRINCIPAL.LOCATION,PRINCIPAL.ITEM,PRINCIPAL.DESCRIPTION, CAST(SUM(PRINCIPAL.OH) AS INT) AS OH, CAJAS
 
-LEFT OUTER JOIN (
-    SELECT 
-        TH.LOCATION,
-        TH.ITEM,
-        TH.USER_NAME,
-         CONVERT(varchar, DATEADD(hour, -6, TH.date_time_stamp), 6) AS FECHA,
-        TH.transaction_Type, 
-        CASE 
-            WHEN TH.transaction_Type = 60 THEN 'Inventory transfer'
-            WHEN TH.transaction_Type = 140 THEN 'Putaway confirmation'
-            WHEN TH.transaction_Type = 40 THEN 'Inventory adjustment'
-            ELSE '' 
-        END AS TIPO,
-        ROW_NUMBER() OVER (PARTITION BY TH.ITEM ORDER BY TH.date_time_stamp ASC) AS RowNum
-    FROM 
-        Transaction_history TH
-    WHERE 
-        TH.warehouse = 'Mariano'
-        AND TH.LOCATION IN ('ALMACENAJE', 'ALMACENAJE-02', 'ALMACENAJE-01')
-        AND TH.transaction_type IN (60, 140, 40)
-) AS THR 
-ON LI.LOCATION = THR.LOCATION  
-   AND LI.ITEM = THR.ITEM 
-   AND THR.RowNum = 1
+FROM (
+  SELECT 
+  ZONAS.WORK_ZONE AS WORK_ZONE,
+  LI.LOCATION AS LOCATION,
+  LI.ITEM AS ITEM,
+  REPLACE(LI.ITEM_DESC, ',', '.') AS DESCRIPTION,
+  CASE
+    WHEN WORK_ZONE IN 
+      ('W-Mar Bodega 1', 'W-Mar Bodega 2', 'W-Mar Bodega 3', 'W-Mar Bodega 4', 'W-Mar Bodega 5', 'W-Mar Bodega 6', 'W-Mar Bodega 7', 'W-Mar Bodega 8', 'W-Mar Bodega 9', 'W-Mar Vinil', 'W-Mar Mayoreo')
+    THEN '1ER PISO'
+
+    WHEN WORK_ZONE IN 
+      ('W-Mar Bodega 10', 'W-Mar Bodega 11', 'W-Mar Bodega 12', 'W-Mar Bodega 13', 'W-Mar Bodega 14', 'W-Mar Bodega 15', 'W-Mar Bodega 16', 'W-Mar Bodega 17', 'W-Mar No Banda')
+    THEN '2DO PISO'
+  ELSE '' 
+  END AS ZONA,
+
+  SUM(LI.ON_HAND_QTY) as OH,
+  CAST((SUM(LI.ON_HAND_QTY)/UOM.conversion_qty) AS INT) AS CAJAS,
+  LI.internal_location_inv
+
+  FROM location_inventory LI
+  INNER JOIN Item_unit_of_measure UOM ON LI.ITEM=UOM.item AND UOM.sequence='2' AND UOM.company='FM'
 
 
-LEFT OUTER JOIN (	 
+  LEFT OUTER JOIN (	 
     SELECT 
     ITEM, WORK_ZONE, LOCATION, AV, OH, AL, IT, SU, NumFila
     FROM (
@@ -74,11 +63,14 @@ LEFT OUTER JOIN (
   ) AS ZONAS ON ZONAS.item = LI.ITEM
 
 
-WHERE 
-    LI.WAREHOUSE = 'Mariano'
-    AND LI.LOCATION IN ('ALMACENAJE', 'ALMACENAJE-02', 'ALMACENAJE-01')
-    -- AND ZONAS.WORK_ZONE = 'W-Mar Bodega 6'
+  WHERE LI.warehouse='Mariano'
+  AND LI.location LIKE 'HOT-%'
+  AND LI.ON_HAND_QTY > 0
 
-ORDER BY 1, FECHA
 
--- WORK_ZONE,LOCATION,ITEM,DESCRIPTION,COMPANY,OH,CAJAS,FECHA,
+  GROUP BY ZONAS.WORK_ZONE, LI.LOCATION, LI.ITEM, LI.ITEM_DESC,WORK_ZONE, LI.ON_HAND_QTY,  LI.internal_location_inv, UOM.conversion_qty
+) AS PRINCIPAL
+
+GROUP BY PRINCIPAL.WORK_ZONE,PRINCIPAL.LOCATION,PRINCIPAL.ITEM,PRINCIPAL.DESCRIPTION, CAJAS
+
+-- WORK_ZONE,LOCATION,ITEM,DESCRIPTION,OH,CAJAS,
