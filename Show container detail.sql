ae25ba5bda515218883930465c9951c8a29150f3
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2025-10-14 08:32:44 -0600

    Update Show container detail.sql

--- Show container detail.sql
+++ Show container detail.sql
@@ -26,7 +26,9 @@ INNER JOIN shipping_container SC ON SD.internal_shipment_num = SC.internal_shipm
 
 WHERE SD.status1 <> '999'
 AND SD.status1 = 600 AND sc.status = 401
+--AND SD.status1 <> sc.status
 -- AND SC.parent_container_id IN ()
 
 -- Detail_internal_shipment_line_num,Header_trailing_sts,Header_leading_sts,Header_internal_shipment_num,Header_shipment_id,Header_shipping_load_num,Detail_item,Detail_status1,Detail_internal_shipment_line_num,Container_status,Container_item,Detail_QTY,Container_location,Container_internal_container_num,Container_parent_container_id,
+
 
