dde23b748f4abd16df0a350320115211586ebbe1
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2025-11-11 07:48:31 -0600

    Update cancelar_inventario.sql

--- Consultas/cancelar_inventario.sql
+++ Consultas/cancelar_inventario.sql
@@ -10,7 +10,9 @@ FROM shipment_detail SD
 INNER JOIN item_location_assignment ILA ON ILA.item = SD.item AND ILA.quantity_um = 'PZ'
 
 WHERE SD.warehouse = 'Mariano'
-AND SD.erp_order LIKE '%-180200'
+  AND SD.status1 <> 999
+  AND SD.erp_order LIKE '%-180200'
 -- AND sd.status1 = 650
 
 -- ELIMINAR PEDIDO Y REGRESAR INVENTARIO
+
