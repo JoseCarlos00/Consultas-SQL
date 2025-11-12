513811789defcbcf5b32a30def7ded8c4b9f658c
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2025-11-12 08:19:25 -0600

    Update Show container detail.sql

--- Show container detail.sql
+++ Show container detail.sql
@@ -1,18 +1,14 @@
 SELECT 
-  -- CONCAT('''', SD.internal_shipment_line_num, '''')
+  -- DISTINCT CONCAT('''', SD.internal_shipment_line_num, '''')
   SD.internal_shipment_line_num  AS  Detail_internal_shipment_line_num1,
-
   SH.trailing_sts AS Header_trailing_sts,
   SH.leading_sts  AS Header_leading_sts,
   SH.internal_shipment_num As  Header_internal_shipment_num,
   SH.shipment_id  AS Header_shipment_id,
   SH.shipping_load_num   AS Header_shipping_load_num,
-
-
   SD.ITEM AS  Detail_item,
   SD.status1 AS  Detail_status1,
   SD.internal_shipment_line_num  AS  Detail_internal_shipment_line_num,
-
   SC.status AS Container_status,  
   SC.item  AS Container_item,
   SD.total_qty AS  Detail_QTY,
@@ -30,6 +26,7 @@ AND SD.status1 = 600 AND sc.status = 401
 --AND SD.status1 <> sc.status AND SC.parent_container_id IN ()
 
 -- Detail_internal_shipment_line_num,Header_trailing_sts,Header_leading_sts,Header_internal_shipment_num,Header_shipment_id,Header_shipping_load_num,Detail_item,Detail_status1,Detail_internal_shipment_line_num,Container_status,Container_item,Detail_QTY,Container_location,Container_internal_container_num,Container_parent_container_id,
+
 
 
 
