2489f27541ceaeb4b30c4156d4073a8a148e3a12
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2025-11-14 11:19:50 -0600

    Update WORK_ZONE conditions in SQL query

--- Cortos/CORTOS_CON_INVENTARIO (2).sql
+++ Cortos/CORTOS_CON_INVENTARIO (2).sql
@@ -12,11 +12,11 @@ SELECT
 
   CASE
     WHEN WORK_ZONE IN 
-      ('W-Mar Bodega 1', 'W-Mar Bodega 2', 'W-Mar Bodega 3', 'W-Mar Bodega 4', 'W-Mar Bodega 5', 'W-Mar Bodega 6', 'W-Mar Bodega 7', 'W-Mar Bodega 8', 'W-Mar Bodega 9','W-Mar Bodega 20', 'W-Mar Vinil', 'W-Mar Mayoreo')
+      ('W-Mar Bodega 1', 'W-Mar Bodega 2', 'W-Mar Bodega 3', 'W-Mar Bodega 4', 'W-Mar Bodega 5', 'W-Mar Bodega 6', 'W-Mar Bodega 7', 'W-Mar Bodega 8', 'W-Mar Bodega 9','W-Mar Bodega 20', 'W-Mar Vinil', 'W-Mar Mayoreo', '')
     THEN '1ER PISO'
 
     WHEN WORK_ZONE IN 
-      ('W-Mar Bodega 10', 'W-Mar Bodega 11', 'W-Mar Bodega 12', 'W-Mar Bodega 13', 'W-Mar Bodega 14', 'W-Mar Bodega 15', 'W-Mar Bodega 16', 'W-Mar Bodega 17', 'W-Mar No Banda', 'W-Mar Bodega 21')
+      ('W-Mar Bodega 10', 'W-Mar Bodega 11', 'W-Mar Bodega 12', 'W-Mar Bodega 13', 'W-Mar Bodega 14', 'W-Mar Bodega 15', 'W-Mar Bodega 16', 'W-Mar Bodega 17', 'W-Mar No Banda', 'W-Mar Bodega 21', 'W-Mar Segundo Piso Reserv')
     THEN '2DO PISO'
   ELSE '' 
   END AS ZONA,
