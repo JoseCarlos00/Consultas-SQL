2c1c26aa60cf8c90de51439fee413768e3ba6c01
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2025-11-17 08:15:35 -0600

    Update Cortos_por_bodega.sql

--- Cortos_por_bodega.sql
+++ Cortos_por_bodega.sql
@@ -14,7 +14,7 @@ SELECT
     WHEN WORK_ZONE IN 
       ('W-Mar Bodega 10', 'W-Mar Bodega 11', 'W-Mar Bodega 12', 'W-Mar Bodega 13', 'W-Mar Bodega 14', 'W-Mar Bodega 15', 'W-Mar Bodega 16', 'W-Mar Bodega 17', 'W-Mar No Banda', 'W-Mar Bodega 21', 'W-Mar Segundo Piso Reserv')
     THEN '2DO PISO'
-  ELSE '' 
+  ELSE '-' 
   END AS PISO
 
 -- consulta_principal
