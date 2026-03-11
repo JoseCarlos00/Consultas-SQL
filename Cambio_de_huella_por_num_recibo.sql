83963b60ee7bcdadfc13faf0ca57c3f69d6e5ff6
José Carlos <101861240+JoseCarlos00@users.noreply.github.com>
2026-03-11 14:17:04 -0600

    Modify UOM conversion_qty calculation with NUM_CAJAS
    Added a variable for number of boxes and updated conversion_qty calculation.

--- Cambio_de_huella_por_num_recibo.sql
+++ Cambio_de_huella_por_num_recibo.sql
@@ -1,8 +1,30 @@
+DECLARE @NUM_CAJAS INT = 10;
+
 UPDATE UOM
-SET UOM.conversion_qty = RD.open_qty
+SET UOM.conversion_qty = UAM.conversion_qty * @NUM_CAJAS
 FROM Item_unit_of_measure UOM
-INNER JOIN RECEIPT_DETAIL RD 
-    ON UOM.item = RD.item 
-   AND UOM.company = RD.company
+INNER JOIN Item_unit_of_measure UAM
+    ON UOM.item = UAM.item
+   AND UOM.company = UAM.company
+   AND UAM.sequence = 2
+INNER JOIN receipt_detail RD
+    ON RD.item = UOM.item
+   AND RD.company = UOM.company
 WHERE UOM.sequence = 3
-  AND RD.INTERNAL_RECEIPT_NUM = 'ITERNAL_RECEIPT_NUMBER';
+  AND RD.internal_receipt_num = 'INTERNAL_RECEIPT_NUMBER';
+
+SELECT 
+    UOM.item,
+    Caja = UAM.conversion_qty,
+    Tarima_actual = UOM.conversion_qty,
+    Tarima_nueva = UAM.conversion_qty * 36
+FROM Item_unit_of_measure UOM
+INNER JOIN Item_unit_of_measure UAM
+    ON UOM.item = UAM.item
+   AND UOM.company = UAM.company
+   AND UAM.sequence = 2
+INNER JOIN receipt_detail RD
+    ON RD.item = UOM.item
+   AND RD.company = UOM.company
+WHERE UOM.sequence = 3
+  AND RD.internal_receipt_num = 'INTERNAL_RECEIPT_NUMBER';
