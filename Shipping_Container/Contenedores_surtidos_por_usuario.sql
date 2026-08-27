bab9f334aa2a666f6324d4bf4295e9ffb5c092e3
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-08-26 18:24:40 -0600

    Update

--- Shipping_Container/Contenedores_surtidos_por_usuario.sql
+++ Shipping_Container/Contenedores_surtidos_por_usuario.sql
@@ -1,53 +1,58 @@
-SELECT
-    ROW_NUMBER() OVER (ORDER BY TH.internal_id) AS NumeroDeFila,
-    TH.internal_id,
-    TH.reference_id,
-    TH.activity_date_time,
-    TH.location,
-    TH.quantity,
-    TH.item,
-    I.description,
-    TH.user_name,
-    TH.work_unit,
-    container.parent_container_id,
-    container.quantity AS container_quantity,
-    container.item AS container_item,
-    container.status,
-    container.shipment_id
-FROM transaction_history TH
-LEFT OUTER JOIN (
-    SELECT
-        SC.parent_container_id,
-        SC.quantity,
-        SC.item,
-        SC.status,
-        SH.shipment_id
-    FROM shipping_container SC
-    INNER JOIN shipment_header SH ON SH.internal_shipment_num = SC.internal_shipment_num
+SELECT 
+  NumeroDeFila,
+  INTERNAL_ID,
+  DATE_TIME,
+  PICK_LOC,
+  TH_QTY,
+  TH_ITEM,
+  USER_NAME,
+  WORK_UNIT,
+  CONTAINER_ID,
+  CONTAINER_QTY,
+  CONTAINER_ITEM,
+  SHIPMENT_ID
 
-    --- CAMBIAR AQUÍ
-    WHERE SH.shipment_id LIKE '361-%'
-     ---- CAMBIAR AQUÍ
+FROM (
+  SELECT
+      ROW_NUMBER() OVER (ORDER BY TH.internal_id) AS NumeroDeFila,
+      TH.internal_id AS INTERNAL_ID,
+      DATEADD(hour, -6, TH.activity_date_time) AS DATE_TIME,
+      TH.location AS PICK_LOC,
+      TH.quantity AS TH_QTY,
+      TH.item AS TH_ITEM,
+      TH.user_name AS USER_NAME,
+      TH.work_unit AS WORK_UNIT,
+      SD.shipment_id AS SHIPMENT_ID,
+      SC.parent_container_id AS CONTAINER_ID,
+      SC.quantity AS CONTAINER_QTY,
+      SC.item AS CONTAINER_ITEM,
+      SC.status AS CONTAINER_STS
 
-) AS container ON container.item = TH.item AND container.quantity = TH.quantity AND container.shipment_id = TH.reference_id
-INNER JOIN item I ON I.item = TH.item AND I.company = 'FM'
-WHERE TH.warehouse = 'Mariano'
+  FROM transaction_history TH
 
-    --- CAMBIAR AQUÍ
-  AND TH.user_name = 'Mariano06'
-  AND TH.reference_id LIKE '361-%'
-  -- AND TH.work_unit = '45102773'
-    ---- CAMBIAR AQUÍ
+  LEFT JOIN shipment_detail SD
+    ON SD.erp_order_line_num = TH.reference_line_num
 
-  AND CAST(TH.activity_date_time AS DATE) = CAST(GETDATE() AS DATE)
-  ---  AND CONVERT(date, DATEADD(hour, -6, TH.activity_date_time)) = 'AAAAMMDD'
+  LEFT JOIN shipping_container SC
+      ON SC.internal_shipment_line_num = SD.internal_shipment_line_num
 
-  AND TH.transaction_type = 130
-  AND TH.direction = 'From'
-  AND TH.work_zone <> 'W-Mar Pick and Drop ELEV'
-  AND TH.work_zone <> 'W-Mar Pick and Drop'
+  WHERE TH.warehouse = 'Mariano'
+    AND TH.reference_id LIKE '3405-%'
+    AND TH.transaction_type = 130
+    AND TH.direction = 'From'
+    AND TH.work_zone <> 'W-Mar Pick and Drop ELEV'
+    AND TH.work_zone <> 'W-Mar Pick and Drop'
+    AND SC.quantity = TH.quantity
+    AND SD.shipment_id = TH.reference_id
 
--- NO,INTERNAL_ID,REFERENCE_ID,ACTIVITY_DATE_TIME,LOCATION,QUANTITY,ITEM,COLOR,USER_NAME,WORK_UNIT,PARENT_CONTAINER_ID,CONTAINER_QTY,CONTAINER_ITEM,CONTAINER_STS,CONTAINER_SHIPMENT_ID,
+      --- CAMBIAR AQUÍ
+    AND TH.user_name = 'LourdesCarreon'
+    -- AND TH.reference_id LIKE '361-%'
+    -- AND TH.work_unit = '45102773'
 
+    AND CAST(TH.activity_date_time AS DATE) = CAST(GETDATE() AS DATE)
+    ---  AND CONVERT(date, DATEADD(hour, -6, TH.activity_date_time)) = 'AAAAMMDD'
 
+) AS RESULT
 
+  -- NO,INTERNAL_ID,REFERENCE_ID,ACTIVITY_DATE_TIME,LOCATION,QUANTITY,ITEM,COLOR,USER_NAME,WORK_UNIT,PARENT_CONTAINER_ID,CONTAINER_QTY,CONTAINER_ITEM,CONTAINER_STS,CONTAINER_SHIPMENT_ID,
